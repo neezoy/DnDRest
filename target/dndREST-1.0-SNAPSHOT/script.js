227144e7d281bd6d7230fcbe12c9ffd93daa683d
AngelofDeath666 <khejser@hotmail.com>
2020-06-08 15:53:51 +0200

    created login page and committed a lot of unversioned files

---
+++ target/dndREST-1.0-SNAPSHOT/script.js
@@ -0,0 +1,4 @@
+function switchPage(page){
+    $("body").load(page);
+}
+
---
+++ target/dndREST-1.0-SNAPSHOT/table.js
@@ -0,0 +1,7 @@
+function insertTable(){
+    var table = document.getElementById("myTable");
+    var row1 = table.insertRow(0);
+    row1.innerHTML = "jdsajkdjsak"
+    table.insertCell(0);
+}
+
